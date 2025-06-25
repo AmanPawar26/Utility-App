f771f5219188e4c344583a60ea7114a2cea46dfd
Your Name <you@example.com>
2025-06-25 16:56:27 +0530

    Imported new routes

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import syncRoutes from './routes/sync.routes.js'
+import propertyRoutes from './routes/property.routes.js';
 
 
 const app = express();
@@ -7,6 +8,7 @@ app.use(express.json());
 
 
 app.use('/api', syncRoutes);
+app.use('/api', propertyRoutes)
 
 
 
